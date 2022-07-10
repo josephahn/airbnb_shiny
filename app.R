c64be11af52e0a72f8115ee168921b6a7c1186d6
Joseph Ahn <joseph.t.ahn@gmail.com>
2022-07-10 09:01:27 -0500

    add neighbourhood select and allow multiselect

--- app.R
+++ app.R
@@ -16,9 +16,15 @@ host_input_choices <- as.list(hosts$host_id)
 names(host_input_choices) <- hosts$host_id_name
 
 boroughs <- data %>%
-  distinct(neighbourhood_group)
+  distinct(neighbourhood_group) %>%
+  arrange(neighbourhood_group)
 # https://stackoverflow.com/questions/10496992/how-to-add-row-on-top-of-data-frame-r
 boroughs <- rbind(c(""), boroughs)
+
+neighbourhoods <- data %>%
+  distinct(neighbourhood) %>%
+  arrange(neighbourhood)
+neighbourhoods <- rbind(c(""), neighbourhoods)
 
 ui <- fluidPage(
   # TODO: improve performance https://shiny.rstudio.com/reference/shiny/latest/selectInput.html
@@ -27,7 +33,12 @@ ui <- fluidPage(
   #             choices = host_input_choices),
   selectInput(inputId = "borough",
               label = "Borough",
-              choices = boroughs$neighbourhood_group),
+              choices = boroughs$neighbourhood_group,
+              multiple = TRUE),
+  selectInput(inputId = "neighbourhood",
+              label = "Neighbourhood",
+              choices = neighbourhoods$neighbourhood,
+              multiple = TRUE),
   dataTableOutput("data")
 )
 
@@ -37,7 +48,8 @@ server <- function(input, output) {
   output$data <- renderDataTable(
     data %>%
       # filter(host_id == input$host) %>%
-      filter(neighbourhood_group == input$borough)
+      filter(neighbourhood_group %in% input$borough) %>%
+      filter(neighbourhood %in% input$neighbourhood)
   )
 }
 
